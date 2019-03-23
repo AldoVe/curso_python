269ade69ae04ceeb64377c1262499d5e6578ed7f
AldoVe <48068114+AldoVe@users.noreply.github.com>
2019-03-22 18:17:51 -0600

    Add files via upload

---
+++ Prueba git/bucle_for_with_range.py
@@ -0,0 +1,28 @@
+
+
+#*-*-*-*-*-*Pïramide con bucle for Mayor a Menor*-*-*-*-*-*#
+
+"""x = int(input("Dame un numero: "))
+y = 0
+print()
+for y in range(x, 0, -1):
+    print(y * "*")
+
+
+#*-*-*-*-*-*Pïramide con bucle for Menor a Mayor*-*-*-*-*-*#
+
+x = int(input("Dame un numero: "))
+y = 0
+print()
+for y in range(0, x):
+    print(y * "*")"""
+
+#*-*-*-*-*-*Cubo con bucle for*-*-*-*-*-*-*
+
+x = int(input("Dame el numero de culumnas: "))
+y = int(input("Dame el numero de filas: "))
+z = 0
+print()
+for z in range(1, y):
+    print(x * "*")
+
